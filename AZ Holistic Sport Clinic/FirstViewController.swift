328f1a9ccf460420f0b37cc2ea15631f6cb4e41f
Μανώλης Γεωργίου <manoliscs@gmail.com>
2018-10-18 15:16:17 +0300

    Initial Commit

---
+++ AZ Holistic Sport Clinic/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AZ Holistic Sport Clinic
+//
+//  Created by Manolis Georgiou on 18/10/2018.
+//  Copyright © 2018 Manolis Georgiou. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
